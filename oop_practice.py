8cdb20111e20483a75b4346a62a94833f5a635b4
LeshaMoklyak <leshamdev@gmail.com>
2025-06-20 00:34:30 +0300

    Доработал классы User и Product

--- oop_practice.py
+++ oop_practice.py
@@ -45,8 +45,89 @@ class BankAccount:
         return f"Счет {self.account_holder}: {self.balance} руб."
     
 
-account = BankAccount("Иван Иванов", 1000)
-account.deposit(500)
-account.withdraw(200)
-print(account.get_balance())  # 1300
-print(account.get_transactions())
+class Product:
+    def __init__(self, name: str, price: float, quant: int):
+        if price <= 0:
+            raise ValueError('Цена должна быть положительным числом')
+        if quant <= 0:
+            raise ValueError('Количество должно быть положительным числом')
+        
+        self.name = name
+        self.price = price
+        self.quant = quant
+    
+    def is_available(self) -> bool:
+        return self.quant > 0
+
+    def change_quantity(self, amount: int) -> int: 
+        if amount > self.quant:
+            raise ValueError(f'Доступно для покупки только  единиц {self.quant} товара')
+        if amount <= 0:
+            raise ValueError('Вы можете купить только положительно число товара')
+        if not self.is_available():
+            raise ValueError('Товар закончился')
+        self.quant = self.quant - amount
+        return self.quant
+
+    def add_quantity(self, amount: int) -> int:
+        if amount < 0:
+            raise ValueError('количество должно быть положительным')
+        self.quant = self.quant + amount
+        return self.quant
+
+    def __str__(self):
+        return f"Название {self.name}, Цена: {self.price}, Остаток: {self.quant}"
+
+
+class User:
+    def __init__(self, user: str, email: str):
+        self.user = user 
+        self.email = email
+
+        self.card: List[Dict] = []
+        self.purchase_history: List[Dict] = []
+
+    def add_to_cart(self, product: Product, quant: int) -> None:
+
+        if not product.is_available():
+            raise ValueError('Товар закончился')
+        if quant < 0: 
+            raise ValueError('Вы можете купить только положительно число товара')
+        if quant > product.quant:
+            raise ValueError(f'Недостаточно товара. Доступно {product.quant}')
+        
+        for item in self.card:
+            if item['product'].name == product.name:
+                item['quantity'] += quant
+                break
+        else:
+            self.card.append({'product': product, 'quantity': quant})
+
+    def remove_to_cart(self, product: Product, quant: int) -> None:
+        if not self.card: 
+            raise ValueError('Корзина пуста')
+        
+        for item in self.card:
+            if not item['product'].name== product.name:
+                raise ValueError('Товар отсутствует у вас в корзине')
+            
+            if item['quantity'] <= product.quant:
+                self.card.remove(item)
+                
+            if item['quantity'] > product.quant:
+                item['quantity'] -= quant
+                
+                  
+
+    def __str__(self):
+        return f'Пользователь: {self.user} Емаил: {self.email} Товаров: {len(self.card)}'
+    
+
+prod1 = Product('arbuz', 25, 10)
+user1 = User('alex', 'email123')
+print(prod1)
+
+user1.add_to_cart(prod1, 2)
+print(user1)
+user1.remove_to_cart(prod1,2)
+print(user1)
